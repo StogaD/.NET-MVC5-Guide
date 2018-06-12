afe07babf2aefa095a7aba83730b126c806b324b
Dawid Stoga PGS <dawid.stoga@gmail.com>
2018-06-12 16:02:10 +0200

    Add changes to BasicController

--- MVCTestApp/Controllers/BaseController.cs
+++ MVCTestApp/Controllers/BaseController.cs
@@ -12,14 +12,38 @@ namespace MvcGuides.Controllers
     {
         public void Execute(RequestContext requestContext)
         {
+            var secretMessage = string.Empty;
+            //  Take values directly or use own helper method
+            var controllerFromUrl = requestContext.RouteData.Values["controller"];
+            var actionrFromUrl = requestContext.RouteData.Values["action"];
 
             var controller = RoutingHelpers.GetRouteVariable<string>(requestContext.RouteData, "Controller");
             var action = RoutingHelpers.GetRouteVariable<string>(requestContext.RouteData, "Action");
 
             var responseMessage = String.Format(" Controller receives the request from URL:  {0}/{1}", controller, action);
 
+            if (action.ToLower().Contains("secret"))
+            {
+                secretMessage += "This messsage is shown due to the SECRET word in the Acrion\n";
+            }
+
+           
+
             // Response can be prepared and sent here without calling any Views
+
+
+
+            if (action.ToLower().Contains("exit"))
+            {
+                requestContext.HttpContext.Response.Redirect("~/Home");
+            }
             requestContext.HttpContext.Response.Write(responseMessage);
+            requestContext.HttpContext.Response.Write(secretMessage);
+
+
+
+
+
         }
     }
 }
