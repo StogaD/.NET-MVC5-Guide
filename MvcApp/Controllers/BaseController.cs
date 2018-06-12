2e2d776158a923b5f67362d5ae3b3eaa3ce17ddd
Dawid Stoga PGS <dawid.stoga@gmail.com>
2018-06-12 15:36:24 +0200

    project structure rebuild v2

---
+++ MvcApp/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using MvcGuides.HelperMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcGuides.Controllers
+{
+    public class BaseController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+
+            var controller = RoutingHelpers.GetRouteVariable<string>(requestContext.RouteData, "Controller");
+            var action = RoutingHelpers.GetRouteVariable<string>(requestContext.RouteData, "Action");
+
+            var responseMessage = String.Format(" Controller receives the request from URL:  {0}/{1}", controller, action);
+
+            // Response can be prepared and sent here without calling any Views
+            requestContext.HttpContext.Response.Write(responseMessage);
+        }
+    }
+}
---
+++ MvcApp/HelperMethods/RoutingHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace MvcGuides.HelperMethods
+{
+    public static class RoutingHelpers
+    {
+        public static T GetRouteVariable<T>(RouteData routeData,  string name)
+        {
+            object result;
+
+            routeData.Values.TryGetValue(name, out result);
+            return (T) result;
+        }
+    }
+}
