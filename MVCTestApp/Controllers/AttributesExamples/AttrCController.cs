53e82f910c8a9c9e5db7ae4e2e172e5a3c37bbed
Dawid Stoga PGS <dawid.stoga@gmail.com>
2018-06-14 13:57:20 +0200

    AuthAttribute

---
+++ MVCTestApp/Controllers/AttributesExamples/AttrCController.cs
@@ -0,0 +1,45 @@
+﻿using MVCTestApp.HelperMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTestApp.Controllers.AttributesExamples
+{
+    public class AttrCController : Controller
+    {
+        // GET: AttrC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ActionName("detale")]
+        public void Details()
+        {
+            this.Send("Details action method was called via [detale] in URL - ActionName attribute was used");
+
+        }
+        [Authorize(Users = "admin")]
+        public void Info()
+        {
+            if (Request.IsAuthenticated)
+            { HttpContext.Response.Write("WithoutFilter Authenticated"); }
+            else
+            {
+                HttpContext.Response.Write("WithoutFilter Not Authenticated");
+            }
+        }
+
+        [OutputCache(Duration = 60)]
+        public void WithGeneralFilter()
+        {
+            if (Request.IsAuthenticated)
+            { HttpContext.Response.Write("WithoutFilter Authenticated"); }
+            else
+            {
+                HttpContext.Response.Write("WithoutFilter Not Authenticated");
+            }
+        }
+    }
+}
---
+++ MVCTestApp/HelperMethods/SendStringResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTestApp.HelperMethods
+{
+    public static class SendStringResponse
+    {
+        public static void Send(this  Controller  controller, string message)
+        {
+            controller.HttpContext.Response.Write(message);
+        }
+    }
+}
